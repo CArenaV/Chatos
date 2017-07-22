74b1bcd190f855862a1647e4d86cd524f8debc13
Shalabh Soni <Shalabh@Shalabhs-MacBook.local>
2017-07-23 02:56:27 +0530

    Initial Commit

---
+++ Chattos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chattos
+//
+//  Created by Shalabh  Soni on 7/23/17.
+//  Copyright © 2017 Shalabh  Soni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
